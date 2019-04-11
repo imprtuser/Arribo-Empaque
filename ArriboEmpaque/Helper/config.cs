4d54cbddebc08a49f0ee984fb57269a4012c8728
Alejandro <israel_alejandro1993@hotmail.com>
2019-04-11 15:38:00 -0500

    cambio de variables en la clase config

--- ArriboEmpaque/Helper/config.cs
+++ ArriboEmpaque/Helper/config.cs
@@ -16,22 +16,22 @@ namespace arrivov2.helper {
         public static String dbLocalName = "ArriboEmpaqueLocalDB";
 
         //Rutas para webservice de pruebas y produccion
-        //public static String pathWS = "http://planning.naturesweet.com/wspackwedge/WSArriboService.asmx";
-        public static String pathWS = "http://192.168.167.8:289/WSArriboService.asmx/";
+        public static String pathWS = "http://planning.naturesweet.com/wspackwedge/WSArriboService.asmx";
+        //public static String pathWS = "http://192.168.167.8:289/WSArriboService.asmx/";
 
 
-        public static String webMethodLogin         = "Login";
-        public static String webMethodPingWS        = "pingToWS";
-        public static String webMethodGetPlants     = "getPlants";
-        public static String webMethodGetFoliosByID = "getInfoFoliosByID";
-        public static String webMethodGetInfoFolio  = "getInfoFolio";
-        public static String webMethodSaveInfoFolio = "saveInfoFolio";
-        public static String webMethodGetFolios     = "getFolios";
+        //public static String webMethodLogin         = "Login";
+        //public static String webMethodPingWS        = "pingToWS";
+        //public static String webMethodGetPlants     = "getPlants";
+        //public static String webMethodGetFoliosByID = "getInfoFoliosByID";
+        //public static String webMethodGetInfoFolio  = "getInfoFolio";
+        //public static String webMethodSaveInfoFolio = "saveInfoFolio";
+        //public static String webMethodGetFolios     = "getFolios";
 
         //VERSION 2 DE LOS WEBMETHODS
-        //public static String webMethodGetInfoFolio  = "getInfoFolioV2";
-        //public static String webMethodSaveInfoFolio = "saveInfoFolioV2";
-        //public static String webMethodGetFolios     = "getFoliosV2";
-        //public static String webMethodGetFoliosByID = "getInfoFoliosByIDV2";
+        public static String webMethodGetInfoFolio = "getInfoFolioV2";
+        public static String webMethodSaveInfoFolio = "saveInfoFolioV2";
+        public static String webMethodGetFolios = "getFoliosV2";
+        public static String webMethodGetFoliosByID = "getInfoFoliosByIDV2";
     }
 }
